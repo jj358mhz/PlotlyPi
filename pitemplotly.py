24fa6354e00f09b50bdaf1a60c290c95f398e133
jj358mhz <jj358mhz@gmail.com>
2014-07-03 08:49:27 -0700

    Update

--- pitemplotly.py
+++ pitemplotly.py
@@ -4,7 +4,6 @@ import plotly.plotly as py # plotly library
 from plotly.graph_objs import Scatter, Layout, Figure # plotly graph objects
 import json # used to parse config.json
 import time # timer functions
-import readadc # helper functions to read ADC from the Raspberry Pi
 import datetime
 
 # Initialize some variables with your credentials
@@ -20,21 +19,28 @@ py.sign_in(username, api_key)
 
 # Initialize your graph (not streaming yet)
 data = [Scatter(x=[],y=[],stream={'token': stream_token, 'maxpoints': 1000})]
-layout = Layout(title='Live graphing from a Raspberry Pi')
-your_graph_url = py.plot(Figure(data=data, layout=layout), filename='Raspi Graph', auto_open=False)
+layout = Layout(title='UCBPD RaspberryPi Temperature')
+your_graph_url = py.plot(Figure(data=data, layout=layout), filename='RaspberryPi 2 Temp', auto_open=False)
 
-# Specify the connected channel for your sensor
-# sensor_pin = 0
+import os
 
-# Initialize the GPIO
-# readadc.initialize()
+cmd = '/opt/vc/bin/vcgencmd measure_temp'
+line = os.popen(cmd).readline().strip()
+
+if "error" in line:
+   print "Error ... is your firmware up-to-date? Run rpi-update"
+else:
+   # line now contains something like: temp=41.2'C
+   # to get the temperature, split on =, and then on '
+
+   temp = line.split('=')[1].split("'")[0]
+   print temp
 
 # Initialize the Plotly Streaming Object
 stream = py.Stream(stream_token)
 stream.open()
 
-# Start looping and streamin'!
+# Start looping and streaming!
 while True:
-    #sensor_data = readadc.readadc(sensor_pin, readadc.PINS.SPICLK, readadc.PINS.SPIMOSI, readadc.PINS.SPIMISO, $
-    stream.write({'x': datetime.datetime.now(), 'y': sensor_data})
-    time.sleep(0.1) # delay between stream posts
+    stream.write({'x': datetime.datetime.now(), 'y': temp})
+    time.sleep(5) # delay between stream posts
