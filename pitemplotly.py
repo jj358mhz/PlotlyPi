b3481afff2c4af692016e836d9aca08b63571b54
jj358mhz <jj358mhz@gmail.com>
2014-07-03 20:53:37 -0700

    Update

--- pitemplotly.py
+++ pitemplotly.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import plotly.plotly as py # plotly library
 from plotly.graph_objs import * # all plotly graph objects
 import json # used to parse config.json
@@ -20,28 +22,28 @@ py.sign_in(username, api_key)
 # Initialize your graph (not streaming yet)
 data = [Scatter(
     x=[],y=[],
+    mode='lines+markers',
     stream={'token': stream_token, 'maxpoints': 1000},
     name='UCBPD')
 ]
 layout = Layout(
-    title='UCBPD RaspberryPi Temperature',
+    title='Raspberry Pi Temperature',
     xaxis={'autorange': True, 'title': 'Time of Day'},
     yaxis={'autorange': True, 'title': 'Degrees (Celsuis)'}
 )
-your_graph_url = py.plot(Figure(data=data, layout=layout), filename='RaspberryPi 2 Temp', auto_open=False)
+your_graph_url = py.plot(Figure(data=data, layout=layout), filename='Raspberry Pi Temp', auto_open=False)
 
 # Acquire internal SoC temperature
 cmd = '/opt/vc/bin/vcgencmd measure_temp'
 line = os.popen(cmd).readline().strip()
 
 if "error" in line:
-   print "Error ... is your firmware up-to-date? Run rpi-update"
+    print "Error ... is your firmware up-to-date? Run rpi-update"
 else:
    # line now contains something like: temp=41.2'C
    # to get the temperature, split on =, and then on '
 
-   temp = line.split('=')[1].split("'")[0]
-   sys.stdout.flush()
+    temp = line.split('=')[1].split("'")[0]
 
 # Initialize the Plotly Streaming Object
 stream = py.Stream(stream_token)
@@ -50,6 +52,4 @@ stream.open()
 # Start looping and streaming!
 while True:
     stream.write({'x': datetime.datetime.now(), 'y': temp})
-    stream.close()
-    sys.stdout.write("\b" * 30)
-    time.sleep(0.1) # delay between stream posts
+    time.sleep(1) # delay between stream posts
